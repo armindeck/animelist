28f612d8dd1ca6c4380981728d867563ec62d0ff
armindeck <dbhsoficial2003@gmail.com>
2026-01-28 17:48:02 -0500

    Campo de busqueda, mejora del menú y degragado en el fondo

---
+++ CHANGELOD.md
@@ -0,0 +1,12 @@
+
+## 2026-01-26
+
+- Agregue el campo de busqueda y mejore el diseño del header.
+    - Ahora hay menu desplegable y funciona bien.
+- Agregue un degradado blanco en el fondo de la pagina.
+
+## 2026-01-25
+
+- Desarrolle el diseño de la página (Header, Hero, Lista y Footer).
+- Al pasar el cursor sobre los poster, se mostrara la información.
+- El hero mostrara un anime con la información y los "enlaces" para seguir, agregar a favorito y agregar a la lista de espera (Construcción).
--- core/views/components/header-view.php
+++ core/views/components/header-view.php
@@ -1,52 +1,76 @@
 <header class="header">
     <div class="header__section">
-        <div class="header__left">
-            <nav class="header__nav">
-                <ul class="header__list">
-                    <li class="header__item">
-                        <a class="logo" href="<?= APP_URL ?? "" ?>" class="header__button">
-                            <span class="icon"><i class="fas fa-meteor"></i></span>
-                            <span class="text"><?= APP_NAME ?? "" ?></span>
-                        </a>
-                    </li>
-                    <li class="header__item">
-                        <a href="<?= APP_URL ?? "" ?>" class="header__button">
-                            <span class="icon"><i class="fas fa-home"></i></span>
-                            <span class="text">Home</span>
-                        </a>
-                    </li>
-                    <li class="header__item">
-                        <a href="<?= APP_URL ?? "" ?>/anime" class="header__button">
-                            <span class="icon"><i class="fas fa-hamburger"></i></span>
-                            <span class="text">Anime</span>
-                        </a>
-                    </li>
-                    <li class="header__item">
-                        <a href="<?= APP_URL ?? "" ?>/list" class="header__button">
-                            <span class="icon"><i class="fas fa-list"></i></span>
-                            <span class="text">List</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+        <input type="checkbox" id="header-menu-check" class="header__check-menu" hidden>
+        <div class="header__header">
+            <div class="header__left">
+                <nav class="header__nav">
+                    <ul class="header__list">
+                        <li class="header__item">
+                            <a class="logo" href="<?= APP_URL ?? "" ?>">
+                                <span class="icon"><i class="fas fa-meteor"></i></span>
+                                <span class="text"><?= APP_NAME ?? "" ?></span>
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </nav>
+            </div>
+            <div class="header__center">
+                <form method="get" action="/search" class="header__search">
+                    <input type="search" name="s" id="search" min="2" placeholder="Search..." required>
+                    <button type="submit"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+            <div class="header__right">
+                <div class="header__list">
+                    <nav class="header__nav">
+                        <label for="header-menu-check" class="header__check-menu header__menu header__item">
+                            <a>
+                                <span class="open">
+                                    <i class="fas fa-bars"></i>
+                                </span>
+                                <span class="close">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                            </a>
+                        </label>
+                    </nav>
+                </div>
+            </div>
         </div>
-        <div class="header__right">
-            <nav class="header__nav">
-                <ul class="header__list">
-                    <li class="header__item">
-                        <a href="<?= APP_URL ?? "" ?>/register" class="header__button">
-                            <span class="icon"><i class="fas fa-user-plus"></i></span>
-                            <span class="text">Register</span>
-                        </a>
-                    </li>
-                    <li class="header__item">
-                        <a href="<?= APP_URL ?? "" ?>/login" class="header__button">
-                            <span class="icon"><i class="fas fa-user"></i></span>
-                            <span class="text">Login</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <nav class="header__nav">
+            <ul class="header__list">
+                <li class="header__item">
+                    <a href="<?= APP_URL ?? "" ?>" class="header__button">
+                        <span class="icon"><i class="fas fa-home"></i></span>
+                        <span class="text">Home</span>
+                    </a>
+                </li>
+                <li class="header__item">
+                    <a href="<?= APP_URL ?? "" ?>/anime" class="header__button">
+                        <span class="icon"><i class="fas fa-hamburger"></i></span>
+                        <span class="text">Anime</span>
+                    </a>
+                </li>
+                <li class="header__item">
+                    <a href="<?= APP_URL ?? "" ?>/list" class="header__button">
+                        <span class="icon"><i class="fas fa-list"></i></span>
+                        <span class="text">List</span>
+                    </a>
+                </li>
+                <li class="header__item">
+                    <a href="<?= APP_URL ?? "" ?>/register" class="header__button">
+                        <span class="icon"><i class="fas fa-user-plus"></i></span>
+                        <span class="text">Register</span>
+                    </a>
+                </li>
+                <li class="header__item">
+                    <a href="<?= APP_URL ?? "" ?>/login" class="header__button">
+                        <span class="icon"><i class="fas fa-user"></i></span>
+                        <span class="text">Login</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </header>
